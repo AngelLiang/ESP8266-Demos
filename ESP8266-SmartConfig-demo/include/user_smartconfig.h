63b52ff97262914af69a9370e9ac7dcb23b952ed
AngelLiang <pl01665077@163.com>
2017-07-03 20:50:00 +0800

    添加SmartConfig示例工程

---
+++ ESP8266-SmartConfig-demo/include/user_smartconfig.h
@@ -0,0 +1,18 @@
+/*
+ * smart_config.h
+ *
+ *  Created on: 2017Äê7ÔÂ3ÈÕ
+ *      Author: Administrator
+ */
+
+#ifndef _USER_SMART_CONFIG_H_
+#define _USER_SMART_CONFIG_H_
+
+#include "user_interface.h"
+#include "smartconfig.h"
+#include "airkiss.h"
+
+extern void smartconfig_done(sc_status status, void *pdata);
+extern void user_smartconfig_init(void);
+
+#endif /* _USER_SMART_CONFIG_H_ */
---
+++ ESP8266-SmartConfig-demo/user/user_main.c
@@ -0,0 +1,96 @@
+/*
+ * ESPRSSIF MIT License
+ *
+ * Copyright (c) 2016 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
+ *
+ * Permission is hereby granted for use on ESPRESSIF SYSTEMS ESP8266 only, in which case,
+ * it is free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "user_smartconfig.h"
+#include "ets_sys.h"
+#include "osapi.h"
+#include "ip_addr.h"
+#include "espconn.h"
+#include "mem.h"
+
+#include "user_interface.h"
+
+
+/******************************************************************************
+ * FunctionName : user_rf_cal_sector_set
+ * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
+ *                We add this function to force users to set rf cal sector, since
+ *                we don't know which sector is free in user's application.
+ *                sector map for last several sectors : ABCCC
+ *                A : rf cal
+ *                B : rf init data
+ *                C : sdk parameters
+ * Parameters   : none
+ * Returns      : rf cal sector
+*******************************************************************************/
+uint32 ICACHE_FLASH_ATTR
+user_rf_cal_sector_set(void)
+{
+    enum flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+void ICACHE_FLASH_ATTR
+user_rf_pre_init(void)
+{
+}
+
+void ICACHE_FLASH_ATTR
+user_init(void)
+{
+    os_printf("SDK version:%s\n", system_get_sdk_version());
+#if 1
+    user_smartconfig_init();
+#else
+	smartconfig_set_type(SC_TYPE_ESPTOUCH); //SC_TYPE_ESPTOUCH,SC_TYPE_AIRKISS,SC_TYPE_ESPTOUCH_AIRKISS
+    wifi_set_opmode(STATION_MODE);
+    smartconfig_start(smartconfig_done);
+#endif
+}
