9b53ced58526822a8c987b32098a8be2bd386161
阏男秀 <pl01665077@163.com>
2017-07-05 16:27:09 +0800

    更新gpio工程代码

--- GPIO_app/user/user_gpio.c
+++ GPIO_app/user/user_gpio.c
@@ -9,6 +9,11 @@
 #include "osapi.h"
 #include "user_interface.h"
 
+#define INT_GPIO	5
+
+#define TIMER_GPIO	0
+#define GPIO_SET_INTERVAL	1000
+
 void user_gpio_interrupt(void *arg)
 {
 	u32 gpio_status;
@@ -16,33 +21,20 @@ void user_gpio_interrupt(void *arg)
 	//clear interrupt status
 	GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, gpio_status);
 
-	u8 gpio2 = GPIO_INPUT_GET(2);
-	if (0==gpio2){
-		os_delay_us(20*1000);		// 延时20ms，去抖动
-		if (0==gpio2){
-			os_printf("GPIO 2: %d\r\n", gpio2);
-		}
+	u8 gpio = GPIO_INPUT_GET(INT_GPIO);
+	os_delay_us(20*1000);		// 延时20ms，去抖动
+	if (gpio==GPIO_INPUT_GET(INT_GPIO)){
+		os_printf("GPIO %d: %d\r\n",INT_GPIO , gpio);
 	}
-
-
 }
 
 void ICACHE_FLASH_ATTR
 user_gpio_interrupt_init(void)
 {
-
-#if 0
-	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0);
-
-	//PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO4_U, FUNC_GPIO4);
-	GPIO_OUTPUT_SET(0, 1);
-	GPIO_OUTPUT_SET(2, 0);
-#endif
-
 	// 管脚功能选择
-	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5);
 	// 设置管脚为输入
-	GPIO_DIS_OUTPUT(2);
+	GPIO_DIS_OUTPUT(INT_GPIO);
 	/*
 	 * GPIO_PIN_INTR_DISABLE - 禁能
 	 * GPIO_PIN_INTR_POSEDGE - 上升沿
@@ -52,7 +44,7 @@ user_gpio_interrupt_init(void)
 	 * GPIO_PIN_INTR_HILEVEL - 高电平
 	 */
 
-	gpio_pin_intr_state_set(GPIO_ID_PIN(2), GPIO_PIN_INTR_NEGEDGE);
+	gpio_pin_intr_state_set(GPIO_ID_PIN(INT_GPIO), GPIO_PIN_INTR_ANYEDGE);
 
 	// 注册 GPIO 中断处理函数
 	ETS_GPIO_INTR_ATTACH(user_gpio_interrupt, NULL);
@@ -66,14 +58,14 @@ user_gpio_interrupt_init(void)
 	os_printf("user_gpio_interrupt_init\r\n");
 }
 
-#define GPIO_SET_INTERVAL	1000
+
 
 void ICACHE_FLASH_ATTR
 gpio_timer_cb(void *arg)
 {
-	u8 gpio0 = GPIO_INPUT_GET(0);
-	GPIO_OUTPUT_SET(0, 1-gpio0);
-	os_printf("GPIO 0: %d\r\n", gpio0);
+	u8 gpio = GPIO_INPUT_GET(TIMER_GPIO);
+	GPIO_OUTPUT_SET(TIMER_GPIO, 1-gpio);
+	//os_printf("GPIO 0: %d\r\n", gpio);
 }
 
 /*
