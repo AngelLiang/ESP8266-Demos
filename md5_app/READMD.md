3621bf2b1df361bf1af29defb82c377ec73297f1
阏男秀 <pl01665077@163.com>
2018-03-12 17:30:17 +0800

    add md5_app

---
+++ md5_app/READMD.md
@@ -0,0 +1,20 @@
+# md5_app
+
+ESP8266使用MD5 hash数据。
+
+相关文件：`user/md5.c`和`include/md5.h`
+
+# 调试信息
+
+上电后会打印如下信息：
+
+```
+...
+*********************************
+before encrypt:admin
+after encrypt 16bit:7a57a5a743894a0e
+before encrypt:admin
+after encrypt 32bit:21232f297a57a5a743894a0e4a801fc3
+*********************************
+...
+```
---
+++ md5_app/include/md5.h
@@ -0,0 +1,47 @@
+#ifndef _MD5_H_
+#define _MD5_H_
+
+typedef struct {
+	unsigned int count[2];
+	unsigned int state[4];
+	unsigned char buffer[64];
+} MD5_CTX;
+
+#define F(x,y,z) ((x & y) | (~x & z))
+#define G(x,y,z) ((x & z) | (y & ~z))
+#define H(x,y,z) (x^y^z)
+#define I(x,y,z) (y ^ (x | ~z))
+#define ROTATE_LEFT(x,n) ((x << n) | (x >> (32-n)))
+
+#define FF(a,b,c,d,x,s,ac) do{ \
+a += F(b, c, d) + x + ac; \
+a = ROTATE_LEFT(a, s); \
+a += b; \
+} while(0)
+
+#define GG(a,b,c,d,x,s,ac) do { \
+a += G(b, c, d) + x + ac; \
+a = ROTATE_LEFT(a, s); \
+a += b; \
+} while(0)
+
+#define HH(a,b,c,d,x,s,ac) do { \
+a += H(b, c, d) + x + ac; \
+a = ROTATE_LEFT(a, s); \
+a += b; \
+}while(0)
+
+#define II(a,b,c,d,x,s,ac) do{ \
+a += I(b, c, d) + x + ac; \
+a = ROTATE_LEFT(a, s); \
+a += b; \
+}while(0)
+
+void MD5Init(MD5_CTX *context);
+void MD5Update(MD5_CTX *context, unsigned char *input, unsigned int inputlen);
+void MD5Final(MD5_CTX *context, unsigned char digest[16]);
+void MD5Transform(unsigned int state[4], unsigned char block[64]);
+void MD5Encode(unsigned char *output, unsigned int *input, unsigned int len);
+void MD5Decode(unsigned int *output, unsigned char *input, unsigned int len);
+
+#endif	/* _MD5_H_ */
