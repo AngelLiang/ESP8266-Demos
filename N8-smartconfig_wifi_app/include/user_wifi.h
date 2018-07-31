df8d14b380ceef2b321696b752260aa1816009f6
阏男秀 <pl01665077@163.com>
2018-07-31 09:30:33 +0800

    添加smartconfig_connect()接口

--- N8-smartconfig_wifi_app/include/user_wifi.h
+++ N8-smartconfig_wifi_app/include/user_wifi.h
@@ -11,6 +11,7 @@
 typedef void (*WifiCallback)(uint8_t);
 
 extern void wifi_connect(WifiCallback cb);
+extern void smartconfig_connect(WifiCallback cb);
 
 extern void wifi_check_init(u16);
 extern void wifi_status_led_init(void);
@@ -22,7 +23,5 @@ extern void wifi_smartconfig_timer_init(void);
 extern void wifi_smartconfig_timer_stop(void);
 
 extern u32 get_station_ip(void);
-
-
 
 #endif /* _USER_WIFI_H_ */
